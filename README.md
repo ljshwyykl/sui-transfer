5ffcf7e518b7a5c9bd87dae08b488c51fcf32287
ljshwyykl <ljs@gmail.com>
2022-10-31 18:15:03 +0800

    add nft

--- README.md
+++ README.md
@@ -14,13 +14,12 @@ https://www.defidaonews.com/article/6783180
 ## 使用
 
 ```
-cargo run $phrase $object_id $count
+cargo run $phrase $count
 
 phrase 转账的的助记词
-object_id https://explorer.devnet.sui.io/ 这个上面去你的地址里面，复制一个
 count  生成的钱包数量
 
 例如 
-cargo run "medal hotel ..." 0x1a656e8a9d75b2508  1
+cargo run "medal hotel ..." 1
 ```
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use chrono::Local;
-use std::{env, fs};
+use serde_json::json;
+
 use std::io::Write;
-use std::str::FromStr;
-use sui_sdk::crypto::{AccountKeystore, FileBasedKeystore, Keystore};
+
+use std::{env, fs};
+use sui_keys::keystore::{AccountKeystore, FileBasedKeystore, Keystore};
 use sui_sdk::types::crypto::SignatureScheme;
 use sui_sdk::types::messages::Transaction;
 use sui_sdk::{
-    types::{base_types::ObjectID, messages::ExecuteTransactionRequestType},
+    json::SuiJsonValue,
+    types::{
+        base_types::ObjectID, base_types::SuiAddress, messages::ExecuteTransactionRequestType,
+        SUI_FRAMEWORK_ADDRESS,
+    },
     SuiClient,
 };
 
@@ -14,19 +20,24 @@ use sui_sdk::{
 async fn main() -> Result<(), anyhow::Error> {
     let args: Vec<String> = env::args().collect();
 
-    let phrase = &args[1];
-    let object_id = &args[2];
+    println!("{}", args.len());
+    if args.len() != 3 {
+        panic!("args error")
+    }
 
-    let total = &args[3].to_string().parse::<usize>().unwrap();
+    let phrase = &args[1];
+    // let object_id = &args[2];
+
+    let total = &args[2].to_string().parse::<usize>().unwrap();
 
     for _i in 0..*total {
-        handle(phrase, object_id).await?;
+        handle(phrase).await?;
     }
 
     Ok(())
 }
 
-async fn handle(phrase_from: &str, object_id: &str) -> Result<(), anyhow::Error> {
+async fn handle(phrase_from: &str) -> Result<(), anyhow::Error> {
     let sui = SuiClient::new_rpc_client("https://fullnode.devnet.sui.io:443", None).await?;
 
     let temp_dir = env::temp_dir();
@@ -38,7 +49,7 @@ async fn handle(phrase_from: &str, object_id: &str) -> Result<(), anyhow::Error>
     println!("address:{},phrase:{},scheme:{:?}", address, phrase, scheme);
 
     fs::create_dir_all("address").unwrap();
-    
+
     let s1 = String::from("address/data");
     let s2 = String::from(".txt");
     let path = format!("{}{}{}", s1, Local::now().timestamp_millis(), s2);
@@ -55,7 +66,11 @@ async fn handle(phrase_from: &str, object_id: &str) -> Result<(), anyhow::Error>
         .import_from_mnemonic(&phrase_from, SignatureScheme::ED25519, None)
         .unwrap();
 
-    let gas_object_id = ObjectID::from_str(&object_id)?;
+    let gas_object_id = get_first_object_id(my_address).await?;
+    println!("my_address:{}", gas_object_id);
+
+    // let gas_object_id = object_id;
+
     let recipient = address;
 
     let transfer_tx = sui
@@ -69,11 +84,96 @@ async fn handle(phrase_from: &str, object_id: &str) -> Result<(), anyhow::Error>
     let transaction_response = sui
         .quorum_driver()
         .execute_transaction(
-            Transaction::new(transfer_tx, signature),
+            Transaction::new(transfer_tx, signature).verify()?,
             Some(ExecuteTransactionRequestType::WaitForLocalExecution),
         )
         .await?;
 
     println!("{:?}", transaction_response);
+
+    let gas_object_id_2 = get_first_object_id(recipient).await?;
+    println!("recipient address:{}", gas_object_id_2);
+
+    create_nft(
+        my_address,
+        ObjectID::from(SUI_FRAMEWORK_ADDRESS),
+        "devnet_nft",
+        "mint",
+        Some(gas_object_id_2),
+        1000,
+        keystore2,
+    )
+    .await?;
+
     Ok(())
+}
+
+async fn create_nft(
+    my_address: SuiAddress,
+    package: ObjectID,
+    module: &str,
+    function: &str,
+    gas: Option<ObjectID>,
+    gas_budget: u64,
+    keystore2: Keystore,
+) -> Result<(), anyhow::Error> {
+    let sui = SuiClient::new_rpc_client("https://fullnode.devnet.sui.io:443", None).await?;
+
+    let args_json = json!([
+        "Qknow NFT",
+        "An NFT created by the Sui Command Line Tool",
+        "ipfs://bafkreibngqhl3gaa7daob4i2vccziay2jjlp435cf66vhono7nrvww53ty"
+    ]);
+    let mut args = vec![];
+    for a in args_json.as_array().unwrap() {
+        args.push(SuiJsonValue::new(a.clone()).unwrap());
+    }
+
+    let transfer_tx = sui
+        .transaction_builder()
+        .move_call(
+            my_address,
+            package,
+            module,
+            function,
+            vec![],
+            args,
+            gas,
+            gas_budget,
+        )
+        .await?;
+
+    let signature = keystore2.sign(&my_address, &transfer_tx.to_bytes())?;
+
+    // Execute the transaction
+    let transaction_response = sui
+        .quorum_driver()
+        .execute_transaction(
+            Transaction::new(transfer_tx, signature).verify()?,
+            Some(ExecuteTransactionRequestType::WaitForLocalExecution),
+        )
+        .await?;
+
+    println!("{:?}", transaction_response);
+    // let nft_id = effects
+    //     .created
+    //     .first()
+    //     .ok_or_else(|| anyhow!("Failed to create NFT"))?
+    //     .reference
+    //     .object_id;
+
+    // println!("{:?}", transaction_response);
+    Ok(())
+}
+
+async fn get_first_object_id(address: SuiAddress) -> Result<ObjectID, anyhow::Error> {
+    let sui = SuiClient::new_rpc_client("https://fullnode.devnet.sui.io:443", None).await?;
+    // let address = SuiAddress::from_str("0x004230a90f543a4993ea3b15954be615f14a71b3")?;
+    let object_refs = sui.read_api().get_objects_owned_by_address(address).await?;
+
+    // let v: Value = serde_json::from_str(objects)?;
+    let object_id = object_refs.first().unwrap().object_id;
+    println!("{}", object_id);
+    // println!("{:?}", objects);
+    Ok(object_id)
 }
